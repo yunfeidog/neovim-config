5d4f497fd10d6dd6116ae73eb210116dac057f26
yunfeidog <1844025705@qq.com>
2024-07-18 01:13:51 +0800

    LazyVim安装lsp代码补全

--- init.lua
+++ init.lua
@@ -1,4 +1,3 @@
 require("options")
 require("keymaps")
 require("plugins")
-require("lsp")
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,64 +1,174 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 
-	-- Vscode-like pictograms
-	{
-		"onsails/lspkind.nvim",
-		event = { "VimEnter" },
-	},
-	-- Auto-completion engine
-	{
-		"hrsh7th/nvim-cmp",
-		dependencies = {
-			"lspkind.nvim",
-			"hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
-			"hrsh7th/cmp-buffer", -- buffer auto-completion
-			"hrsh7th/cmp-path", -- path auto-completion
-			"hrsh7th/cmp-cmdline", -- cmdline auto-completion
-		},
-		config = function()
-			require("config.nvim-cmp")
-		end,
-	},
-	-- Code snippet engine
-	{
-		"L3MON4D3/LuaSnip",
-		version = "v2.*",
-	},
-	-- LSP manager
+    -- Vscode-like pictograms
     {
-	    "williamboman/mason.nvim",
-	    "williamboman/mason-lspconfig.nvim",
-	    "neovim/nvim-lspconfig"
+        "onsails/lspkind.nvim",
+        event = { "VimEnter" },
+    },
+    -- Auto-completion engine
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            "lspkind.nvim",
+            "hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
+            "hrsh7th/cmp-buffer",   -- buffer auto-completion
+            "hrsh7th/cmp-path",     -- path auto-completion
+            "hrsh7th/cmp-cmdline",  -- cmdline auto-completion
+        },
+        config = function()
+            require("config.nvim-cmp")
+        end,
+    },
+    -- Code snippet engine
+    {
+        "L3MON4D3/LuaSnip",
+        version = "v2.*",
+    },
+    -- LSP manager
+    {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+        "neovim/nvim-lspconfig"
+    },
+    {
+        event = "VeryLazy",
+        "neovim/nvim-lspconfig",
+        dependencies = { "williamboman/mason-lspconfig.nvim" },
+    },
+    {
+        event = "VeryLazy",
+        "jose-elias-alvarez/null-ls.nvim",
+        config = function()
+            local null_ls = require("null-ls")
+
+            local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+            null_ls.setup({
+                sources = {
+                    null_ls.builtins.formatting.stylua,
+                    null_ls.builtins.formatting.black,
+                },
+                on_attach = function(client, bufnr)
+                    if client.supports_method("textDocument/formatting") then
+                        vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+                        vim.api.nvim_create_autocmd("BufWritePre", {
+                            group = augroup,
+                            buffer = bufnr,
+                            callback = function()
+                                -- on 0.8, you should use vim.lsp.buf.format({ bufnr = bufnr }) instead
+                                vim.lsp.buf.format({ bufnr = bufnr })
+                            end,
+                        })
+                    end
+                end,
+            })
+        end,
     },
 
     -- 文件搜索
     {
-		cmd = "Telescope",
-		keys = {
-			{ "<leader>p", ":Telescope find_files<CR>", desc = "find files" },
-			{ "<leader>P", ":Telescope live_grep<CR>", desc = "grep file" },
-			{ "<leader>rs", ":Telescope resume<CR>", desc = "resume" },
-			{ "<leader>q", ":Telescope oldfiles<CR>", desc = "oldfiles" },
-		},
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.1",
-		-- or                              , branch = '0.1.1',
-		dependencies = { "nvim-lua/plenary.nvim" },
-	},
+        cmd = "Telescope",                                                      -- 懒加载，直到输入Telescope 的时候才会加载
+        keys = {
+            { "<leader>p",  ":Telescope find_files<CR>", desc = "find files" }, --CR就是回车
+            { "<leader>P",  ":Telescope live_grep<CR>",  desc = "grep file" },
+            { "<leader>rs", ":Telescope resume<CR>",     desc = "resume" },
+            { "<leader>q",  ":Telescope oldfiles<CR>",   desc = "oldfiles" },
+        },
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.1",
+        -- or                              , branch = '0.1.1',
+        dependencies = { "nvim-lua/plenary.nvim" },
+    },
+})
+
+
+
+-- Use LspAttach autocommand to only map the following keys
+-- after the language server attaches to the current buffer
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+        -- Enable completion triggered by <c-x><c-o>
+        vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+        -- Buffer
+        -- See `:help vim.lsp.*` for documentation on any of the below functions
+        local opts = { buffer = ev.buf }
+        vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+        vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<leader>wa", vim.lsp.buf.add_workspace_folder, opts)
+        vim.keymap.set("n", "<leader>wr", vim.lsp.buf.remove_workspace_folder, opts)
+        vim.keymap.set("n", "<leader>wl", function()
+            print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+        end, opts)
+        vim.keymap.set("n", "<leader>D", vim.lsp.buf.type_definition, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+        vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>f", function()
+            vim.lsp.buf.format({ async = true })
+        end, opts)
+    end,
+})
+
+
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+-- Set up lspconfig.
+
+require("lspconfig").lua_ls.setup({
+    capabilities = capabilities,
+    settings = {
+        Lua = {
+            runtime = {
+                -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+                version = "LuaJIT",
+            },
+            diagnostics = {
+                -- Get the language server to recognize the `vim` global
+                globals = { "vim", "hs" },
+            },
+            workspace = {
+                checkThirdParty = false,
+                -- Make the server aware of Neovim runtime files
+                library = {
+                    vim.api.nvim_get_runtime_file("", true),
+                    "/Applications/Hammerspoon.app/Contents/Resources/extensions/hs/",
+                    vim.fn.expand("~/lualib/share/lua/5.4"),
+                    vim.fn.expand("~/lualib/lib/luarocks/rocks-5.4"),
+                    "/opt/homebrew/opt/openresty/lualib",
+                },
+            },
+            completion = {
+                callSnippet = "Replace",
+            },
+            -- Do not send telemetry data containing a randomized but unique identifier
+            telemetry = {
+                enable = false,
+            },
+        },
+    },
+})
+
+require("lspconfig").pyright.setup({
+    capabilities = capabilities,
 })
