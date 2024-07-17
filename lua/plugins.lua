5d552be7c92488e934fd4a44203b5c307f4d4c71
yunfeidog <1844025705@qq.com>
2024-07-17 14:52:39 +0800

    LazyVim安装CMP代码补全插件

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -14,5 +14,31 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({})
+require("lazy").setup({
 
+	-- Vscode-like pictograms
+	{
+		"onsails/lspkind.nvim",
+		event = { "VimEnter" },
+	},
+	-- Auto-completion engine
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"lspkind.nvim",
+			"hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
+			"hrsh7th/cmp-buffer", -- buffer auto-completion
+			"hrsh7th/cmp-path", -- path auto-completion
+			"hrsh7th/cmp-cmdline", -- cmdline auto-completion
+		},
+		config = function()
+			require("config.nvim-cmp")
+		end,
+	},
+	-- Code snippet engine
+	{
+		"L3MON4D3/LuaSnip",
+		version = "v2.*",
+	},
+
+})
