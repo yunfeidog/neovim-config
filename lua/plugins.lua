d43886cf88e19414830f416e61f84ff452c67cbf
yunfeidog <1844025705@qq.com>
2024-07-17 22:15:19 +0800

    LazyVim安装Telescope文件搜索插件OC

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -45,5 +45,20 @@ require("lazy").setup({
 	    "williamboman/mason.nvim",
 	    "williamboman/mason-lspconfig.nvim",
 	    "neovim/nvim-lspconfig"
-    }
+    },
+
+    -- 文件搜索
+    {
+		cmd = "Telescope",
+		keys = {
+			{ "<leader>p", ":Telescope find_files<CR>", desc = "find files" },
+			{ "<leader>P", ":Telescope live_grep<CR>", desc = "grep file" },
+			{ "<leader>rs", ":Telescope resume<CR>", desc = "resume" },
+			{ "<leader>q", ":Telescope oldfiles<CR>", desc = "oldfiles" },
+		},
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.1",
+		-- or                              , branch = '0.1.1',
+		dependencies = { "nvim-lua/plenary.nvim" },
+	},
 })
