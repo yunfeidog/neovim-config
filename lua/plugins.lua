abe0edbbbfc68500c32a20c84027e95acf80b951
yunfeidog <1844025705@qq.com>
2024-07-18 13:17:03 +0800

    feat:增加nerdtree目录树插件

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -134,6 +134,26 @@ require("lazy").setup({
             require("Navigator").setup()
         end,
     },
+    {
+        keys = {
+            { "<leader>t", ":NERDTreeToggle<CR>", desc = "toggle nerdtree" },
+            { "<leader>l", ":NERDTreeFind<CR>",   desc = "nerdtree find" },
+        },
+        cmd = { "NERDTreeToggle", "NERDTree", "NERDTreeFind" },
+        "preservim/nerdtree",
+        config = function()
+            vim.cmd([[
+" enable line numbers
+let NERDTreeShowLineNumbers=1
+" make sure relative line numbers are used
+autocmd FileType nerdtree setlocal relativenumber
+		]])
+        end,
+        dependencies = {
+            "Xuyuanp/nerdtree-git-plugin",
+            "ryanoasis/vim-devicons",
+        },
+    },
 })
 
 
