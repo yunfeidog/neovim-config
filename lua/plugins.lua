a0e3add0a13beb4472a4c1efcddf6b59320f36b9
yunfeidog <1844025705@qq.com>
2024-07-18 10:05:07 +0800

    增加一些LSP的配置

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -21,18 +21,26 @@ require("lazy").setup({
         "onsails/lspkind.nvim",
         event = { "VimEnter" },
     },
-    -- Auto-completion engine
+    -- Auto-completion engine 自动补全插件
     {
+        event = "VeryLazy",
         "hrsh7th/nvim-cmp",
         dependencies = {
-            "lspkind.nvim",
-            "hrsh7th/cmp-nvim-lsp", -- lsp auto-completion
-            "hrsh7th/cmp-buffer",   -- buffer auto-completion
-            "hrsh7th/cmp-path",     -- path auto-completion
-            "hrsh7th/cmp-cmdline",  -- cmdline auto-completion
+            "neovim/nvim-lspconfig",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
+            "hrsh7th/cmp-cmdline",
+            "hrsh7th/nvim-cmp",
+            "L3MON4D3/LuaSnip",
         },
+    },
+    -- 给补全的函数加括号
+    {
+        "windwp/nvim-autopairs",
+        event = "VeryLazy",
         config = function()
-            require("config.nvim-cmp")
+            require("nvim-autopairs").setup({})
         end,
     },
     -- Code snippet engine
@@ -79,6 +87,9 @@ require("lazy").setup({
             })
         end,
     },
+    {
+        "folke/neodev.nvim",
+    },
 
     -- 文件搜索
     {
@@ -98,6 +109,7 @@ require("lazy").setup({
 
 
 
+-- =============================================================null-ls格式化的配置=============================================================
 -- Use LspAttach autocommand to only map the following keys
 -- after the language server attaches to the current buffer
 vim.api.nvim_create_autocmd("LspAttach", {
@@ -129,11 +141,16 @@ vim.api.nvim_create_autocmd("LspAttach", {
     end,
 })
 
+-- =============================================================null-ls格式化的配置=============================================================
 
-
+-- =============================================================lspconfig的配置=============================================================
 require("mason").setup()
 require("mason-lspconfig").setup()
 -- Set up lspconfig.
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
+require("neodev").setup({
+    -- add any options here, or leave empty to use the default settings
+})
 
 require("lspconfig").lua_ls.setup({
     capabilities = capabilities,
@@ -172,3 +189,102 @@ require("lspconfig").lua_ls.setup({
 require("lspconfig").pyright.setup({
     capabilities = capabilities,
 })
+
+-- =============================================================lspconfig的配置=============================================================
+
+-- =============================================================自动补全cmp的配置=============================================================
+local has_words_before = function()
+    unpack = unpack or table.unpack
+    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
+local luasnip = require("luasnip")
+
+
+local cmp = require("cmp")
+-- 自动补全后给函数加空格
+local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+
+
+cmp.setup({
+    snippet = {
+        -- REQUIRED - you must specify a snippet engine
+        expand = function(args)
+            require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+            -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+            -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+        end,
+    },
+    window = {
+        -- completion = cmp.config.window.bordered(),
+        -- documentation = cmp.config.window.bordered(),
+    },
+    mapping = cmp.mapping.preset.insert({
+        ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_next_item()
+                -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
+                -- they way you will only jump inside the snippet region
+            elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+            elseif has_words_before() then
+                cmp.complete()
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+
+        ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+                cmp.select_prev_item()
+            elseif luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+            else
+                fallback()
+            end
+        end, { "i", "s" }),
+        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
+        ["<C-f>"] = cmp.mapping.scroll_docs(4),
+        ["<C-Space>"] = cmp.mapping.complete(),
+        ["<C-c>"] = cmp.mapping.abort(),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+        { name = "nvim_lsp" },
+        { name = "luasnip" }, -- For luasnip users.
+        -- { name = 'ultisnips' }, -- For ultisnips users.
+        -- { name = 'snippy' }, -- For snippy users.
+    }, {
+        { name = "buffer" },
+    }),
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype("gitcommit", {
+    sources = cmp.config.sources({
+        { name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+    }, {
+        { name = "buffer" },
+    }),
+})
+
+-- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline({ "/", "?" }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+        { name = "buffer" },
+    },
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(":", {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+        { name = "path" },
+    }, {
+        { name = "cmdline" },
+    }),
+})
+-- =============================================================自动补全cmp的配置=============================================================
