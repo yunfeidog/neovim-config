f68d723dda7378fd9f9e19e3d1a0395b2318bde2
yunfeidog <1844025705@qq.com>
2024-09-28 08:32:09 +0800

    feat:删除无用代码

--- init.lua
+++ init.lua
@@ -1,3 +1,16 @@
 require("core.options")
 require("core.keymaps")
 require("config.lazy")
+
+local args = vim.api.nvim_get_vvar("argv")
+-- embed
+if #args > 2 then
+else
+	require("persistence").load({ last = true })
+end
+
+-- persistence on start
+vim.api.nvim_set_hl(0, "@lsp.type.variable.lua", { link = "Normal" })
+vim.api.nvim_set_hl(0, "Identifier", { link = "Normal" })
+vim.api.nvim_set_hl(0, "TSVariable", { link = "Normal" })
+vim.api.nvim_set_hl(0, "@variable.builtin.vim", { link = "Normal" })
